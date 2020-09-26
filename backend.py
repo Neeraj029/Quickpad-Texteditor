316975cdb045abf9354dd924a1c2ba063d2b5989
Neerajsamshette <neeraj.samshette@gmail.com>
2020-09-26 19:30:41 +0530

    Initial Commit

---
+++ backend.py
@@ -0,0 +1,17 @@
+from main import Frontend
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+from os.path import basename
+from os import system
+from tkinter.messagebox import askokcancel, askyesno
+from tkinter import PhotoImage
+
+frontend = Frontend()
+
+class Functions():
+    
+    
+            
+ 
+
+function1 = Functions()
